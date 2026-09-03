417b424c32bf78138928c4ced6258d9fd1fb6a5f
Mano Kumar S <saibagavath.mano@gmail.com>
2026-09-03 23:54:30 +0530

    Time: 0 ms (100%), Space: 19.9 MB (82.05%) - CodeHub

--- leetcode/0035-search-insert-position/0035-search-insert-position.py
+++ leetcode/0035-search-insert-position/0035-search-insert-position.py
@@ -1,6 +1,5 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        ans = len(nums)
         left = 0
         right = len(nums) - 1
 
@@ -9,12 +8,10 @@ class Solution:
 
             if nums[mid] == target:
                 return mid
-
             elif nums[mid] > target:
-                ans = mid
                 right = mid - 1
-
             else:
                 left = mid + 1
-
-        return ans
+        
+        return left
+    
