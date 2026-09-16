f019763b9535f83434dfcc134a76a2530c3b1d0c
Mano Kumar S <saibagavath.mano@gmail.com>
2026-09-16 23:51:48 +0530

    Time: 4 ms (4.21%), Space: 19.3 MB (25.08%) - CodeHub

--- leetcode/0874-backspace-string-compare/0874-backspace-string-compare.py
+++ leetcode/0874-backspace-string-compare/0874-backspace-string-compare.py
@@ -1,22 +1,44 @@
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-
-        string = []
-
-        for i in range(len(s)):
-            if s[i] == "#":
-                if string:
-                    string.pop()
-            else:
-                string.append(s[i])
-
-        str_sec = []
-
-        for i in range(len(t)):
-            if t[i] == "#":
-                if str_sec:
-                    str_sec.pop()
-            else:
-                str_sec.append(t[i])
-
-        return string == str_sec
+        s = list(s)
+        t = list(t)
+        left = len(s) - 1
+        left2 = len(t) - 1
+        
+        while left >= 0 or left2 >= 0:
+            # skip logic for s
+            skip = 0
+            while left >= 0:
+                if s[left] == '#':
+                    skip += 1
+                    left -= 1
+                elif skip > 0:
+                    skip -= 1
+                    left -= 1
+                else:
+                    break
+            
+            # skip logic for t
+            skip2 = 0
+            while left2 >= 0:
+                if t[left2] == '#':
+                    skip2 += 1
+                    left2 -= 1
+                elif skip2 > 0:
+                    skip2 -= 1
+                    left2 -= 1
+                else:
+                    break
+            
+            # now compare landed positions
+            if left >= 0 and left2 >= 0:
+                if s[left] != t[left2]:
+                    return False
+            elif left >= 0 or left2 >= 0:
+                # one string still has a real char, other ran out
+                return False
+            
+            left -= 1
+            left2 -= 1
+        
+        return True
