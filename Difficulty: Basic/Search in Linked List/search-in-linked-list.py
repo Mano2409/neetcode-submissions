f4269969e398ec87ede368272f491d06cb940a5c
Mano Kumar S <saibagavath.mano@gmail.com>
2026-09-19 10:12:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Search in Linked List/search-in-linked-list.py
@@ -0,0 +1,17 @@
+'''Structure of Linked List Node
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+'''
+class Solution:
+    def searchKey(self, head, key):
+        temp = head
+
+        while temp != None:
+            if temp.data == key:
+                return True
+
+            temp = temp.next
+
+        return False
